4871b42c6e320300ea38ee2f12444901566c35f5
Craig A Wiswell <cwiswell63@gmail.com>
2019-04-02 21:15:51 -0400

    Added material UI to project. Create Home page component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
+import 'typeface-roboto';
 
-class App extends Component {
+import Home from './home/home.component'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <Home></Home>
+      </MuiThemeProvider>
     );
   }
 }
-
-export default App;
