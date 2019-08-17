ca57320d548c95fb68d81457693bc31527e9e33f
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-17 18:06:34 -0400

    Update router to stateless component

--- src/menu/router.tsx
+++ src/menu/router.tsx
@@ -1,18 +1,17 @@
-import React, { Component, Fragment } from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '../home/home.component';
 import PageNotFound from '../pageNotFound/page-not-found.component';
 
-export default class Router extends Component{
 
-    render(){
-        return(
-            <Switch>                
-                <Route exact path="/" component={Home} />
-                <Route path="/home" component={Home} />
-                <Route component={PageNotFound} />
-            </Switch>
-        )
-    }
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
+            <Route component={PageNotFound} />
+        </Switch>
+    )
 }
+export default Router; 
