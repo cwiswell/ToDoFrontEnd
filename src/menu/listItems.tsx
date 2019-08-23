d5ee2193ea26c2c5f76ff420c0fbe2701fbec40e
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-22 20:45:19 -0400

    Pass link component into listitem

--- src/menu/listItems.tsx
+++ src/menu/listItems.tsx
@@ -4,29 +4,27 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import ListIcon from '@material-ui/icons/List';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const mainListItems = (
   <div>
-    <Link to="/home" style={{textDecoration: 'none'}}>
-      <ListItem button >
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        < ListItemText primary="Home" />
-      </ListItem>
-    </Link>
+    <ListItem button component={Link} to="/home" >
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      < ListItemText primary="Home" />
+    </ListItem>
     <ListItem button>
       <ListItemIcon>
         <ListIcon />
       </ListItemIcon>
       <ListItemText primary="Active To Do Lists" />
-    </ListItem>   
+    </ListItem>
     <ListItem button>
       <ListItemIcon>
         <ListIcon />
       </ListItemIcon>
       <ListItemText primary="Closed To Do Lists" />
-    </ListItem>   
+    </ListItem>
   </div>
 );
