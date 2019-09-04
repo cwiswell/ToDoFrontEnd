73e8917abdc6099a0c6197040e58621125f42900
Craig A Wiswell <cwiswell63@gmail.com>
2019-09-03 20:25:10 -0400

    Add edit button to active to do card

--- src/home/to-do-card.tsx
+++ src/home/to-do-card.tsx
@@ -2,18 +2,29 @@ import React, { Fragment } from 'react';
 import Card from '@material-ui/core/Paper';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
 import ToDo from '../toDoComponent/to-do.component';
 import IToDoCard from './to-do-card-props.interface';
 
 const ToDoCard: React.FC<IToDoCard> = (props) => {
+
+    const editAction = props.checkEnable ? 
+        ( <CardActions disableSpacing>
+            <IconButton aria-label="edit to do list">
+                <EditIcon />
+            </IconButton>
+        </CardActions>) :
+        null;
+
     return (
         <Fragment>
             <Card >
-                <CardHeader title={props.cardTitle} subheader={props.toDoList.title}>
-                </CardHeader>
+                <CardHeader title={props.cardTitle} subheader={props.toDoList.title} action={editAction} />
                 <CardContent>
                     <ToDo data={props.toDoList} enabled={props.checkEnable} />
-                </CardContent>
+                </CardContent>     
             </Card>
         </Fragment>
     );
