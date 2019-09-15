98c20e516d3ec6ae8a2584b043bc9650f190a94d
Craig A Wiswell <cwiswell63@gmail.com>
2019-09-14 22:34:17 -0400

    Change test data for previous todos

--- src/libs/to-do-data-service.ts
+++ src/libs/to-do-data-service.ts
@@ -2,8 +2,9 @@ import ToDoList from "../toDoComponent/todo-list.interface";
 import ToDoItem from "../toDoComponent/todo.class";
 
 const toDoData: ToDoItem[] = [{text:"Some Item",checked: false}, {text: "Do Dishes", checked: true}, {text: "Yard Work",checked: false}, {text: "Make dinner", checked: false}];
+const prevToDoData: ToDoItem[] = [{text:"Stuff",checked: true}, {text: "Cat litter", checked: true}, {text: "Dishes",checked: true}, {text: "Make dinner", checked: true}, {text: "Laundry", checked: true}];
 const toDoList: ToDoList = { title: "Monday ToDo List", toDoItems: toDoData };
-const prevToDoList: ToDoList = { title: "Sunday To Do List", toDoItems: toDoData };
+const prevToDoList: ToDoList = { title: "Sunday To Do List", toDoItems: prevToDoData };
 
 export function GetActiveToDos(): Promise<ToDoList[] | null> {
     return new Promise<ToDoList[] | null>((resolve, reject) => {
