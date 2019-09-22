8189c6436ecbbfb1c6cec205f2e995b83d0355e3
Craig A Wiswell <cwiswell63@gmail.com>
2019-09-21 20:04:05 -0400

    Add paper to todo form

--- src/toDoComponent/to-do-form.component.tsx
+++ src/toDoComponent/to-do-form.component.tsx
@@ -1,8 +1,14 @@
 import React, { Fragment } from 'react';
+import { Paper, Typography } from '@material-ui/core';
 
 const ToDoForm: React.FC = () => {
     return (
         <Fragment>
+            <Paper>
+                <Typography variant="h5" component="h3">
+                    Edit To Do List
+                </Typography>
+            </Paper>
         </Fragment>
     );
 }
