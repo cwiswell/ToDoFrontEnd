12e615a4eb5d11c3062ddc1b74fabec0adb74945
Craig A Wiswell <cwiswell63@gmail.com>
2019-09-25 20:11:06 -0400

    Update todolist interface with id property

--- src/libs/to-do-data-service.ts
+++ src/libs/to-do-data-service.ts
@@ -3,12 +3,12 @@ import ToDoItem from "../toDoComponent/todo.interface";
 
 const toDoData: ToDoItem[] = [{text:"Some Item",checked: false}, {text: "Do Dishes", checked: true}, {text: "Yard Work",checked: false}, {text: "Make dinner", checked: false}];
 const prevToDoData: ToDoItem[] = [{text:"Stuff",checked: true}, {text: "Cat litter", checked: true}, {text: "Dishes",checked: true}, {text: "Make dinner", checked: true}, {text: "Laundry", checked: true}];
-const toDoList: ToDoList = { title: "Monday ToDo List", toDoItems: toDoData, active:true };
-const prevToDoList: ToDoList = { title: "Sunday To Do List", toDoItems: prevToDoData, active:false };
+const toDoList: ToDoList = { title: "Monday ToDo List", toDoItems: toDoData, active:true , id: 1};
+const prevToDoList: ToDoList = { title: "Sunday To Do List", toDoItems: prevToDoData, active:false, id: 5 };
 
 export function GetActiveToDos(): Promise<ToDoList[] | null> {
     return new Promise<ToDoList[] | null>((resolve, reject) => {
-        let data: ToDoList[] = [toDoList, {title: "Other list", toDoItems: toDoData, active:true},{title: "Weekly To Dos", toDoItems: toDoData, active:true}];
+        let data: ToDoList[] = [toDoList, {title: "Other list", toDoItems: toDoData, active:true, id: 2},{title: "Weekly To Dos", toDoItems: toDoData, active:true, id: 3}];
         resolve(data);
     });
 }
--- src/toDoComponent/todo-list.interface.ts
+++ src/toDoComponent/todo-list.interface.ts
@@ -1,6 +1,7 @@
 import ToDoItem from "./todo.interface";
 
 export default interface ToDoList{
+    id: number;
     active: boolean;
     title: string;
     toDoItems: ToDoItem[];
