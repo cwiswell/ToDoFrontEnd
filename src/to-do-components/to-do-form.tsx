298bc098b2c1a099590890b86b82bad96bab0544
Craig A Wiswell <cwiswell63@gmail.com>
2019-09-29 15:06:28 -0400

    Add button to to do form

--- src/to-do-components/to-do-form.tsx
+++ src/to-do-components/to-do-form.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, Typography, Button } from '@material-ui/core';
 import { GetToDo } from '../libs/to-do-data-service';
 import ToDoList from '../interfaces/todo-list';
+import AddIcon from '@material-ui/icons/Add';
 
 type TParams = { id?: number }
 
@@ -17,28 +18,35 @@ type ToDoFormProps = {
 }
 
 const ToDoForm: React.FC<ToDoFormProps> = (props) => {
-    const [data, setData] = useState<ToDoList | null>(null);
-    
+    const [data, setData] = useState<ToDoList | null>({} as ToDoList);
+
     const id = props.match.params.id;
     const title = id === undefined ? "Add To Do List" : `Edit To Do List ${id}`;
-    
-    if(props.match.params.id !== undefined){
-        GetToDo(props.match.params.id).then((data)=>{
+
+    if (props.match.params.id !== undefined) {
+        GetToDo(props.match.params.id).then((data) => {
             setData(data);
         });
     }
 
-    const body = data == null && id !== undefined ? 
-        (<Typography> No to do list with id</Typography>):
-        (<Typography> Data goes here</Typography>)
+    const body = data == null && id !== undefined ?
+        (<Typography> No to do list with id</Typography>) :
+        (<Typography> Data goes here</Typography>);
+
+    const addItem = () => {
+        console.log("add Item");
+    };
 
     return (
         <Fragment>
-            <Paper style={{padding:'10px'}}>
-                <Typography variant="h5" component="h3" style={{textAlign: 'center'}}>
+            <Paper style={{ padding: '10px' }}>
+                <Typography variant="h5" component="h3" style={{ textAlign: 'center' }}>
                     {title}
                 </Typography>
                 {body}
+                <Button variant="contained" color="primary" onClick={addItem} >
+                    Add Item <AddIcon />
+                </Button>
             </Paper>
         </Fragment>
     );
