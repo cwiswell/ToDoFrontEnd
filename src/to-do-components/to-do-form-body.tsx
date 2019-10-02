8796e3095b18ff9e2b183219a12aea83603b3bae
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-01 22:20:23 -0400

    Add inputs to to-do-form-body

--- src/to-do-components/to-do-form-body.tsx
+++ src/to-do-components/to-do-form-body.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import ToDoList from '../interfaces/todo-list';
+import { Typography, Input, Divider } from '@material-ui/core';
 
 type ToDoFormItemProps = {
     data: ToDoList;
@@ -8,10 +9,19 @@ type ToDoFormItemProps = {
 const ToDoFormBody: React.FC<ToDoFormItemProps> = (props) => {
     const [data, setData] = useState<ToDoList | null>({} as ToDoList);
 
+    const title = data == null ?
+    (<Input defaultValue="To Do Title"  inputProps={{'aria-label': 'to do title',}}/>):
+    (<Input value={data.title} defaultValue="To Do Tite"  inputProps={{'aria-label': 'to do title',}}/>);
+
+    const body = data == null ? 
+    (<Typography> No data</Typography>) : 
+    data.toDoItems.map((item, key) => (<Input key={key} value={item.text} defaultValue="To Do Item"  inputProps={{'aria-label': 'to do item',}}/>));
 
     return (
         <Fragment>
-
+            {title}
+            <Divider variant="middle" />
+            {body}
         </Fragment>
     );
 }
