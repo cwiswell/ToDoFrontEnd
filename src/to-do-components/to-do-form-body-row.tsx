c48fe796b104d534425022a91d96ebdfcaafe4d8
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-02 21:57:39 -0400

    Add form body row component

---
+++ src/to-do-components/to-do-form-body-row.tsx
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { Input, Grid, Checkbox } from '@material-ui/core';
+import ToDoItem from '../interfaces/todo';
+
+type ToDoFormItemProps = {
+    data: ToDoItem | null;
+}
+
+const ToDoFormBodyItem: React.FC<ToDoFormItemProps> = (props) => {
+    const [data, setData] = useState<ToDoItem | null>(null);
+
+    useEffect(() => {
+        setData(props.data);
+    }, [props.data])
+
+    return (
+        <Fragment>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Checkbox
+                        checked={data == null ? false : data.checked}
+                        tabIndex={-1}
+                        disableRipple
+                    />
+                </Grid>
+                <Grid item xs>
+                    <Input
+                        value={data == null ? null : data.text}
+                        defaultValue="To Do Item"
+                        inputProps={{ 'aria-label': 'to do item', }}
+                        style={{ width: '100%' }} />
+                </Grid>
+                <Grid item xs>
+                </Grid>
+            </Grid>
+        </Fragment>
+    );
+}
+
+export default ToDoFormBodyItem;
