30f28e9ecaab11f11dee76dc28803bc318f3d631
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-10 15:05:22 -0400

    Add new to do item input form

--- src/to-do-components/to-do-form.tsx
+++ src/to-do-components/to-do-form.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Paper, Typography, Button, Input, Divider } from '@material-ui/core';
+import { Paper, Typography, Button, Input, Divider, Grid } from '@material-ui/core';
 import { GetToDo } from '../libs/to-do-data-service';
 import ToDoList from '../interfaces/todo-list';
 import AddIcon from '@material-ui/icons/Add';
@@ -19,7 +19,8 @@ type ToDoFormProps = {
 }
 
 const ToDoForm: React.FC<ToDoFormProps> = (props) => {
-    const [data, setData] = useState<ToDoList | null>({title: ''} as ToDoList);
+    const [data, setData] = useState<ToDoList | null>({ title: '' } as ToDoList);
+    const [newText, setNewText] = useState<string>('');
 
     const id = props.match.params.id;
     const title = id === undefined ? "Add To Do List" : `Edit To Do List ${id}`;
@@ -31,19 +32,23 @@ const ToDoForm: React.FC<ToDoFormProps> = (props) => {
     }
 
     const toDoTitle = data === null || data === undefined ?
-    (<Input defaultValue="To Do Title"  inputProps={{'aria-label': 'to do title',}}/>):
-    (<Input value={data.title}  inputProps={{'aria-label': 'to do title',}}/>);
+        (<Input defaultValue="To Do Title" inputProps={{ 'aria-label': 'to do title', }} />) :
+        (<Input value={data.title} inputProps={{ 'aria-label': 'to do title', }} />);
 
     const body = data === null && id !== undefined ?
         (<Typography> No to do list with id</Typography>) :
-        (<ToDoFormBody data={data == null ? null : data.toDoItems}/>);
+        (<ToDoFormBody data={data == null ? null : data.toDoItems} />);
 
     const addItem = () => {
-        
+
     };
 
     const saveChanges = () => {
 
+    };
+
+    const onTextChange = (e: React.FormEvent<HTMLTextAreaElement|HTMLInputElement>) => {
+        setNewText(e.currentTarget.value);
     };
 
     return (
@@ -54,13 +59,27 @@ const ToDoForm: React.FC<ToDoFormProps> = (props) => {
                 </Typography>
                 <form>
                     {toDoTitle}
-                    <Divider variant="middle" style={{margin: '20px 0'}} />
-                    <Button variant="contained" color="primary" onClick={addItem} >
-                        Add Item <AddIcon />
-                    </Button>
-                    <Divider variant="middle" style={{margin: '20px 0'}} />
+                    <Divider variant="middle" style={{ margin: '20px 0' }} />
+                    <Grid container spacing={1}>
+                        <Grid item xs>
+                            <Input
+                                value={newText}
+                                onChange={onTextChange}
+                                inputProps={{ 'aria-label': 'to do item', }}
+                                style={{ width: '100%' }} />
+                        </Grid>
+                        <Grid item xs={2} style={{ textAlign: 'center' }}>
+                            <Button variant="contained" color="primary" onClick={addItem} >
+                                Add Item <AddIcon />
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+
+
+                    <Divider variant="middle" style={{ margin: '20px 0' }} />
                     {body}
-                    <Divider variant="middle" style={{margin: '20px 0'}} />
+                    <Divider variant="middle" style={{ margin: '20px 0' }} />
 
                     <Button variant="contained" color="primary" onClick={saveChanges} >
                         Save
