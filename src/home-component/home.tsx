999589dbe7e8e7c6bb6fb0dd65742bfae0d8f266
Craig A Wiswell <cwiswell63@gmail.com>
2019-11-10 10:05:13 -0500

    Add button to create new on home page

--- src/home-component/home.tsx
+++ src/home-component/home.tsx
@@ -6,6 +6,7 @@ import ActiveToDosCard from './active-to-dos-card';
 import { GetMostRecentActiveToDo, GetRecentlyClosedToDo } from '../libs/to-do-data-service';
 import { withStyles } from '@material-ui/styles';
 import { RouteComponentProps, withRouter } from 'react-router';
+import { Button } from '@material-ui/core';
 
 const styles = (theme: any) => ({
   root: {
@@ -36,6 +37,10 @@ const Home: React.FC<IHomeProps> = (props) => {
     props.history.push(`/todo/edit/${id}`);
   }
 
+  const createToDoList = () =>{
+    props.history.push('/todo/create');
+  }
+
   GetMostRecentActiveToDo().then((data) => {
     setCurrentToDoList(data);
   });
@@ -48,6 +53,7 @@ const Home: React.FC<IHomeProps> = (props) => {
 
   return (
     <Fragment>
+      <Button onClick={createToDoList}>Add New ToDo List</Button>
       <Grid container className={classes.root} spacing={2}>
         <Grid item md={4}>
           <ToDoCard cardTitle="Recent To Do List" toDoList={currentToDoList} checkEnable={true} editFunction={editToDoList} />
